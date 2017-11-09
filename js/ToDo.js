68e2f709e01b7a11c20a17bdfb699abd82a51302
iamdlynm <dbum.35@hotmail.com>
2017-11-09 14:08:05 -0700

    Added bootstrap trash icon and css.

--- js/ToDo.js
+++ js/ToDo.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-
+    //adds list item when button is clicked
     $('#addItem').on('click', addItem);
+    $('#todos').on('change', '.completeItem', completeItem);
+    //click event on deleteitem, then starts deleteitem function
+    $('#todos').on('click', '.deleteItem', deleteItem);
 
     function addItem() {
+        //collects new item and stores in this variable
         var newTodoText = $('#newTodo').val();
-        $('#todos').append('<ul>' + newTodoText + '</ul>');
+        //adds new item to the list
+        $('#todos').append('<ul><input class = "completeItem" type = "checkbox">' + newTodoText + '<span class = "glyphicon glyphicon-trash deleteItem"></span></ul>');
         $('#newTodo').val("");
 
+    }
+
+    function deleteItem(){
+        //chooses the ul and removes when you click on trash
+        $(this).parent().remove();
+
+    }
+
+    function completeItem(){
+    //chooses the ul parent and markes done
+        $(this).parent().toggleClass('done');
     }
 });
 
